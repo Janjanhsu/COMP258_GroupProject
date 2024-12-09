7bfa890e2f4944e0922833d03209ae97fce961d3
winghk00 <winghk@gmail.com>
2024-12-08 22:18:39 -0500

    updated web app

--- app/app.py
+++ app/app.py
@@ -10,6 +10,7 @@ import tensorflow as tf
 from tensorflow.keras.models import load_model
 from PIL import Image
 import numpy as np
+import datetime
 
 app = Flask(__name__)
 model = load_model('my_model.keras')
@@ -18,13 +19,19 @@ model = load_model('my_model.keras')
 def index():
     return render_template('index.html')
 
+class_names = ['angry', 'disgust', 'fear', 'happy', 'neutral', 'sad', 'surprise']
+class_emoji = ['😡','😓','😱','😁','🙂','😭','😮']
+
 @app.route('/upload', methods=['POST'])
 def upload():
     file = request.files['file']
     image = Image.open(file.stream)
     prediction = predict_image(image)
-    print("Upload prediction: ", prediction)
-    return f'Prediction: {prediction}'
+    prediction_name = class_names[prediction]
+    emoji_result = class_emoji[prediction]
+    print("Capture prediction: ", prediction_name, emoji_result)
+    now = datetime.datetime.now()
+    return f'Prediction: {prediction_name} {emoji_result} - last update: {now.time()}'
 
 @app.route('/capture', methods=['POST'])
 def capture():
@@ -32,18 +39,25 @@ def capture():
     image_data = base64.b64decode(data['image'].split(',')[1])
     image = Image.open(io.BytesIO(image_data))
     prediction = predict_image(image)
-    print("Capture prediction: ", prediction)
-    return f'Prediction: {prediction}'
+    prediction_name = class_names[prediction]
+    emoji_result = class_emoji[prediction]
+    print("Capture prediction: ", prediction_name, emoji_result)
+    now = datetime.datetime.now()
+    return f'Prediction: {prediction_name} {emoji_result} - last update: {now.time()}'
 
-
+def Normalize(image):
+  image = tf.cast(image/255.0, tf.float32)
+  return image
 
 def predict_image(image):
     image = image.convert("RGB")
+    #image = image.convert("L")
     image = image.resize((48, 48))
-    image = np.array(image) / 255.0
+    #image = np.array(image) / 255.0
     image = np.expand_dims(image, axis=0)
     prediction = model.predict(image)
     return np.argmax(prediction, axis=1)[0]
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    #app.run(debug=True)
+    app.run(host='0.0.0.0')
